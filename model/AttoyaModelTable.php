4c258f81a0aea491888a4235971d654ec73b1983
TravSchan <accounts@travschan.com>
2026-02-11 09:27:01 +0700

    Model

---
+++ model/AttoyaModelTable.php
@@ -0,0 +1,27 @@
+<?php
+
+
+#
+# Attoya Model Table Class
+#
+class AttoyaModelTable extends AttoyaDatabaseOrm {
+
+  protected static $_database_name  = null;
+  protected static $_database_table = null;
+  protected static $_database_model = null;
+
+
+
+
+  #
+  # Init
+  #
+  public static function init() {
+
+  }
+
+
+}
+
+
+
